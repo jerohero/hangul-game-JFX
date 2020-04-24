7a02aad94f9aed817b4b4049f0fb02faef5f89ce
Jeroen <jegbol01@gmail.com>
2020-04-24 23:36:48 +0200

    weee

---
+++ src/sample/Background.java
@@ -0,0 +1,9 @@
+package sample;
+
+import java.awt.*;
+
+public class Background extends Rectangle {
+    public Background(){
+        super(500, 700);
+    }
+}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -6,59 +6,73 @@ import java.util.Iterator;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.geometry.Point2D;
 import javafx.scene.Node;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
 import javafx.scene.paint.Color;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
+import sample.LevelData;
+
 public class Main extends Application {
 
     private HashMap<KeyCode, Boolean> keys = new HashMap<KeyCode, Boolean>();
+
     private ArrayList<Node> platforms = new ArrayList<Node>();
+    private ArrayList<Node> enemies = new ArrayList<Node>();
 
     private Pane appRoot = new Pane();
     private Pane gameRoot = new Pane();
     private Pane uiRoot = new Pane();
 
     private Node player;
-    private Point2D playerVelocity = new Point2D(0,0);
+    private Point2D playerVelocity = new Point2D(0, 0);
     private boolean canJump = true;
 
     private int levelWidth;
 
-    private void initContent() {
-        Rectangle bg = new Rectangle(1280, 720);
+    private boolean dialogEvent = false, running = true;
 
-        levelWidth = LevelData.LEVEL1[0].length() * 60; // 60 is blocksize
+    private void initContent() {
+        Image bgImg = new Image("sample/resources/img/bg.jpg");
+        ImageView bg = new ImageView(bgImg);
+
+        levelWidth = LevelData.LEVEL1[0].length() * 60;             // 60 is block size ofzo
 
         for (int i = 0; i < LevelData.LEVEL1.length; i++) {
             String line = LevelData.LEVEL1[i];
-            for (int j = 0; j < line.length(); j++){
-                switch(line.charAt(j)){
+            for (int j = 0; j < line.length(); j++) {
+                switch (line.charAt(j)) {
                     case '0':
                         break;
                     case '1':
-                        Node platform = createEntity(j*60, i*60, 60, 60, Color.BROWN);
+                        Node platform = createEntity(j*60, i*60, 60, 60, "sample/resources/img/floortile.png");
                         platforms.add(platform);
+                        break;
+                    case '2':
+                        Node enemy = createEntity(j*60, i*60, 60, 60, "sample/resources/img/enemy1.png");
+                        enemies.add(enemy);
+                        break;
+                    case '3':
+                        player = createEntity(j*60, i*60, 40, 40, "sample/resources/img/enemy1.png");
                         break;
                 }
             }
         }
 
-        player = createEntity(0, 600, 40, 40, Color.BLUE);
+//        player.translateXProperty().addListener((obs, old, newValue) -> {
+//            int offset = newValue.intValue();
+//
+//            if (offset > 640 && offset < levelWidth - 640) {
+//                gameRoot.setLayoutX(-(offset - 640));
+//            }
+//        });
 
-        player.translateXProperty().addListener((obs, old, newValue) -> {
-            int offset = newValue.intValue();
-
-            if(offset > 640 && offset < levelWidth - 640){
-                gameRoot.setLayoutX(-(offset - 640)); //collision met linkermuur
-            }
-        });
         appRoot.getChildren().addAll(bg, gameRoot, uiRoot);
     }
 
@@ -66,39 +80,108 @@ public class Main extends Application {
         if (isPressed(KeyCode.W) && player.getTranslateY() >= 5) {
             jumpPlayer();
         }
-        if (isPressed(KeyCode.A) && player.getTranslateX() >= 5){
+
+        if (isPressed(KeyCode.A) && player.getTranslateX() >= 5) {
             movePlayerX(-5);
         }
-        if (isPressed(KeyCode.D) && player.getTranslateX() + 40 <= levelWidth- 5) {
+
+        if (isPressed(KeyCode.D) && player.getTranslateX() + 40 <= levelWidth - 5) {
             movePlayerX(5);
         }
-        if (playerVelocity.getY() < 10){
+
+        if (playerVelocity.getY() < 10) {
             playerVelocity = playerVelocity.add(0, 1);
         }
+
         movePlayerY((int)playerVelocity.getY());
+
+        for (Node enemy : enemies) {
+            if (player.getBoundsInParent().intersects(enemy.getBoundsInParent())) {
+                enemy.getProperties().put("alive", false);
+                dialogEvent = true;
+                running = false;
+            }
+        }
+
+        for (Iterator<Node> it = enemies.iterator(); it.hasNext(); ) {
+            Node coin = it.next();
+            if (!(Boolean)coin.getProperties().get("alive")) {
+                it.remove();
+                gameRoot.getChildren().remove(coin);
+            }
+        }
     }
 
     private void movePlayerX(int value) {
         boolean movingRight = value > 0;
+
+        for (int i = 0; i < Math.abs(value); i++) {
+            for (Node platform : platforms) {
+                if (player.getBoundsInParent().intersects(platform.getBoundsInParent())) {       //collision met platform
+                    if (movingRight) {
+                        if (player.getTranslateX() + 40 == platform.getTranslateX()) {             //collides rechts
+                            return;                                                                //kan niet meer bewegen
+                        }
+                    }
+                    else {                                                                      //height player
+                        if (player.getTranslateX() == platform.getTranslateX() + 60) {
+                            return;
+                        }
+                    }
+                }
+            }
+            player.setTranslateX(player.getTranslateX() + (movingRight ? 1 : -1));
+        }
     }
 
-    private void movePlayerY(int value) {}
+    private void movePlayerY(int value) {
+        boolean movingDown = value > 0;
+
+        for (int i = 0; i < Math.abs(value); i++) {
+            for (Node platform : platforms) {
+                if (player.getBoundsInParent().intersects(platform.getBoundsInParent())) {
+                    if (movingDown) {
+                        if (player.getTranslateY() + 40 == platform.getTranslateY()) {
+                            player.setTranslateY(player.getTranslateY() - 1);
+                            canJump = true;
+                            return;
+                        }
+                    }
+                    else {
+                        if (player.getTranslateY() == platform.getTranslateY() + 60) {
+                            return;
+                        }
+                    }
+                }
+            }
+            player.setTranslateY(player.getTranslateY() + (movingDown ? 1 : -1));
+        }
+    }
 
     private void jumpPlayer() {
-        if (canJump){
+        if (canJump) {
             playerVelocity = playerVelocity.add(0, -30);
             canJump = false;
         }
     }
 
-//    private Node createEntity(int x, int y, int w, int h, Color color) {}
+    private Node createEntity(int x, int y, int w, int h, String path) {
+        Image img = new Image(path);
+        ImageView entity = new ImageView(img);
+        entity.setTranslateX(x);
+        entity.setTranslateY(y);
+        entity.getProperties().put("alive", true);
+
+        gameRoot.getChildren().add(entity);
+        return entity;
+    }
 
     private boolean isPressed(KeyCode key) {
         return keys.getOrDefault(key, false);
     }
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         initContent();
 
         Scene scene = new Scene(appRoot);
@@ -111,11 +194,33 @@ public class Main extends Application {
         AnimationTimer timer = new AnimationTimer() {
             @Override
             public void handle(long now) {
-                update();
+                if (running) {
+                    update();
+                }
+
+                if (dialogEvent) {
+                    dialogEvent = false;
+                    keys.keySet().forEach(key -> keys.put(key, false));
+
+                    GameDialog dialog = new GameDialog();
+                    dialog.setOnCloseRequest(event -> {
+                        if (dialog.isCorrect()) {
+                            System.out.println("Correct");
+                        }
+                        else {
+                            System.out.println("Wrong");
+                        }
+
+                        running = true;
+                    });
+                    dialog.open();
+                }
             }
         };
         timer.start();
     }
 
-    public static void main(String[] args){ launch(args); }
+    public static void main(String[] args) {
+        launch(args);
+    }
 }
